72c9e14735ebc70b27e7d3271fbf4bf53d70dfab
ppxb <317842449@qq.com>
2026-03-31 15:36:45 +0800

    chore: update unicorn rules

--- internal/lint-configs/oxlint-config/src/configs/unicorn.ts
+++ internal/lint-configs/oxlint-config/src/configs/unicorn.ts
@@ -2,12 +2,19 @@ import type { OxlintConfig } from 'oxlint'
 
 const unicorn: OxlintConfig = {
   rules: {
-    'unicorn/consistent-function-scoping': 'off',
     'unicorn/no-process-exit': 'error',
+    'unicorn/prefer-module': 'error',
+    'unicorn/no-invalid-fetch-options': 'error',
+    'unicorn/no-invalid-remove-event-listener': 'error',
+    'unicorn/no-thenable': 'error',
+    'unicorn/no-unnecessary-await': 'error',
+    'unicorn/no-useless-fallback-in-spread': 'error',
+    'unicorn/prefer-string-starts-ends-with': 'error',
+    'unicorn/prefer-set-size': 'warn',
+    'unicorn/consistent-function-scoping': 'off',
     'unicorn/no-single-promise-in-promise-methods': 'off',
     'unicorn/no-useless-spread': 'off',
-    'unicorn/prefer-global-this': 'off',
-    'unicorn/prefer-module': 'error'
+    'unicorn/prefer-global-this': 'off'
   }
 }
 
